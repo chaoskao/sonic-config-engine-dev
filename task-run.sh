cfe4e995e6e85d02539a89dd55d9a3c84cb4cff7
Chaos Kao <chaos_kao@accton.com>
2019-03-19 17:14:29 +0800

    add main script

---
+++ task-run.sh
@@ -0,0 +1,18 @@
+case "$1" in
+  install)
+        
+        ;;
+  build)
+        
+        ;;
+  
+  check)
+        
+        ;;
+  *)
+        echo "Usage: $N {install|build}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
